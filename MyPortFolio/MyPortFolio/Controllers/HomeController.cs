358cd026d4209428303497762b83ee28a803bdb4
Saed Fathi <saed.fathi@gmail.com>
2023-01-02 21:33:52 +0330

    create View contact

--- MyPortFolio/MyPortFolio/Controllers/HomeController.cs
+++ MyPortFolio/MyPortFolio/Controllers/HomeController.cs
@@ -6,19 +6,14 @@ namespace MyPortFolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+       
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
